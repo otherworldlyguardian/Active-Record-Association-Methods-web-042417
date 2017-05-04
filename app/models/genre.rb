2b817835eefe4d50c4797c4d3d8dbc8172f1267d
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-05-04 14:00:32 -0400

    Drake in da house

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,14 +3,22 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    # return the number of songs in a genre
+    self.songs.size
   end
 
   def artist_count
-    # return the number of artists associated with the genre
+    art = []
+    self.songs.each do |song|
+      art << song.artist if !art.include?(song.artist)
+    end
+    art.size
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+    art = []
+    self.songs.each do |song|
+      art << song.artist.name if !art.include?(song.artist.name)
+    end
+    art
   end
 end
